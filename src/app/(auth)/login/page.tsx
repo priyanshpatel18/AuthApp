c3a18ba0fe2e018ad267a3672554d4f9694c22d8
priyanshpatel18 <priyansh.180804@gmail.com>
2024-05-01 23:43:52 +0530

    removed AuthLayout

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,13 @@
 import SignIn from "@/components/Signin";
+import authOptions from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
 export default async function Login() {
+  const session = await getServerSession(authOptions);
+  if (session?.user) {
+    redirect("/");
+  }
+
   return <SignIn />;
 }
